a7083fcf34c5007b123273f73130ddde56908183
GabrielDevgt <roldangabriel002@gmail.com>
2024-09-03 11:47:02 -0600

    PA1 inicial body, finished form only work css

--- renderer.js
+++ renderer.js
@@ -2,4 +2,9 @@ document.addEventListener("DOMContentLoaded", function (){
     document.getElementById('triggerDiv').addEventListener('click', function() {
         document.getElementById('myModal').style.display = 'block';
     });
+
+    document.getElementById('closeBtn').addEventListener('click', function() {
+        document.getElementById('myModal').style.display = 'none';
+    });
+
 })
