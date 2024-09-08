f4ed67b920b83a5877cd0003a4928cc738fd3553
GabrielDevgt <roldangabriel002@gmail.com>
2024-09-07 22:39:46 -0600

    css mymodal3 search patient

--- renderer.js
+++ renderer.js
@@ -15,4 +15,13 @@ document.addEventListener("DOMContentLoaded", function (){
     document.getElementById('closeBtn1').addEventListener('click', function() {
         document.getElementById('myModal1').style.display = 'none';
     });
+
+    document.getElementById('triggerDiv2').addEventListener('click', function() {
+        document.getElementById('myModal2').style.display = 'block';
+    });
+
+    document.getElementById('closeBtn3').addEventListener('click', function() {
+        document.getElementById('myModal2').style.display = 'none';
+    });
+
 })
