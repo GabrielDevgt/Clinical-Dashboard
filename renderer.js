8a4dfa3de12f9a828f9018882d6af8fe7e445875
GabrielDevgt <roldangabriel002@gmail.com>
2024-09-07 19:32:37 -0600

    Initial preelimination data base for modal2

--- renderer.js
+++ renderer.js
@@ -7,4 +7,12 @@ document.addEventListener("DOMContentLoaded", function (){
         document.getElementById('myModal').style.display = 'none';
     });
 
+    document.getElementById('triggerDiv1').addEventListener('click', function() {
+        document.getElementById('myModal1').style.display = 'block';
+        window.api.send('get-all-patients');
+    });
+
+    document.getElementById('closeBtn1').addEventListener('click', function() {
+        document.getElementById('myModal1').style.display = 'none';
+    });
 })
